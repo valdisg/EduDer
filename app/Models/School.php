26967a26ed488bebb72f60163ee40574db644d6c
karlis.pokkers <karlis.pokkers@pasts.lv>
2023-06-04 12:16:19 +0300

    School model addon

--- app/Models/School.php
+++ app/Models/School.php
@@ -7,10 +7,28 @@ use Illuminate\Database\Eloquent\Model;
 
 class School extends Model
 {
+    public const SCHOOL_TYPE_GENERAL_SECONDARY_SCHOOL = 6;
+    public const SCHOOL_TYPE_STATE_HIGH_SCHOOL = 9;
+
+    public const  SCHOOL_TYPES = [
+        self::SCHOOL_TYPE_GENERAL_SECONDARY_SCHOOL => 'Vispārizglītojošā vidusskola',
+        self::SCHOOL_TYPE_STATE_HIGH_SCHOOL => 'Valsts ģimnāzija',
+    ];
+
     use HasFactory;
 
     protected $fillable = [
         'school_title',
         'address',
+        'type',
+        'type_name',
+        'school_data_id',
+        'coordinates',
+        'registration_number',
+        'phone_number',
+        'email',
+        'url',
+        'image',
+        'manager',
     ];
 }
